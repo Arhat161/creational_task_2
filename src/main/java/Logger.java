94a0354ca8d43724810fd864ca9d882324c3fa69
Arhat161 <Arhat161@users.noreply.github.com>
2022-04-21 17:49:17 +0300

    Initial commit

---
+++ src/main/java/Logger.java
@@ -0,0 +1,28 @@
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Logger {
+    protected int num = 1;
+    protected List<String> logList = new ArrayList<>();
+
+    private static Logger logger;
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if (logger == null) logger = new Logger();
+        return logger;
+    }
+
+    public void log(String msg) {
+        String timeStamp = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(Calendar.getInstance().getTime());
+        logList.add("[" + timeStamp + " " + num++ + "] " + msg);
+    }
+
+    public void showLog() {
+        logList.forEach((n) -> System.out.println(n));
+    }
+}
